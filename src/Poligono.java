fb4eabee5346cb5346f7caf898b45e955d698e2e
LuigiZannella <zannellaluigi@outlook.com>
2022-06-28 16:06:45 +0200

    Finito Eserczio

---
+++ src/Poligono.java
@@ -0,0 +1,9 @@
+public interface Poligono {
+	//Creare un’interfaccia Poligono con due metodi: calcolaPerimetro() e calcolaArea()
+
+	
+	//creo metodo calcola perimetro e calcola area
+	public abstract int calcolaPerimetro();
+	public abstract int calcolaArea();
+
+}
---
+++ src/Quadrato.java
@@ -0,0 +1,40 @@
+public class Quadrato implements Poligono {
+	
+	//attributi
+	private int lato;
+	
+
+	//costruttore
+	public Quadrato(int lato) {
+		super();
+		
+		this.lato = lato;
+		
+	}
+	
+	
+	@Override
+	public int calcolaPerimetro() {
+		int perimetro =lato*4;
+		return perimetro;
+	}
+
+	
+
+	@Override
+	public int calcolaArea() {
+		int area = lato * lato;
+		return area;
+	}
+
+
+	@Override
+	public String toString() {
+		return "L'area del quadrato è:  " + calcolaArea() + " - " + "Il perimetro del quadrato è: " + calcolaPerimetro();  
+				
+	}
+
+
+	
+
+}
